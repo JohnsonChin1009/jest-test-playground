869780e0394769c0f25c3e0eeb0fa8d25c2032ba
johnsonchin <johnsonchin1009@gmail.com>
2026-01-12 02:28:29 +0800

    feat: update the logic

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-import "dotenv";
+import "dotenv/config";
 
 function addTwoNumbers(x: number, y: number) {
   return x + y;
@@ -21,7 +21,7 @@ function divideTwoNumbers(x: number, y: number) {
 }
 
 function loadSecret() {
-  return process.env.SECRET_API_KEY == null;
+  return process.env.SECRET_API_KEY != null;
 }
 
 export {
